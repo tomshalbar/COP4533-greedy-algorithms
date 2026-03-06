25e48e0083e439c5a9afdfd08658ce6c03ad344a
ttavera234 <ttavera234@gmail.com>
2026-03-05 19:02:23 -0500

    Cleaned up the main file so that the software now outputs the data in the desired format (reads from input -> outputs into file). Also made the code more legible/understandable

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
+from argparse import ArgumentParser
 from pathlib import Path
+
 from src.fifo import Fifo as fifo
 from src.lru import Lru as lru
 from src.optff import OPTFF as optff
 
-input_dir_path = Path("./inputs/")
-
 
 def read_file(path: Path):
-    """returns a tuple or the int cache capacity and list(int) requests read from a file"""
+    """returns a tuple of the int cache capacity and list(int) requests read from a file"""
     with open(path, "r") as f:
         meta = f.readline().strip()
         content = f.readline().strip()
@@ -18,47 +18,72 @@ def read_file(path: Path):
         return cache_capacity, requests
 
 
-def test_fifo_and_lru_caches(caches, requests):
-    """ Gets the hits and misses from the lru and fifo cache eviction policies """
-    results = dict()
-    for cache in caches:
-        results[cache.__class__.__name__] = []
+def get_cache_misses(cache: lru | fifo | optff, requests: list[int] | None = None) -> int:
+    """
+        Returns the number of cache misses for the LRU, FIFO, and OPTFF cache eviction policies
+    """
+    miss: int = 0
+    if isinstance(cache, lru) or isinstance(cache, fifo):
         for r in requests:
-            trial = cache.request(r)
-            results[cache.__class__.__name__].append(trial)
-
-    return results
-
-
-def calculate_fifo_and_lru_stats(results):
-    """ Sums up the hits and misses from the lru and fifo cache eviction policies """
-    stats = dict()
-    for cache in results:
-        miss = 0
-        hit = 0
-        for trial in results[cache]:
-            if trial == "miss":
+            res = cache.request(r)
+            if res == "miss":
                 miss += 1
-            else:
-                hit += 1
+    elif isinstance(cache, optff):
+        miss = cache.process_requests()
+    else:
+        print("Error: Cache eviction policy is not supported!!")
+        return -1
 
-        #stats[cache] = [miss, hit]
-        stats[cache] = miss
-
-    return stats
+    return miss
 
 
-def main():
-    for i, path in enumerate(input_dir_path.iterdir()):
-        cache_capacity, requests = read_file(path)
-        caches = [fifo(cache_capacity), lru(cache_capacity), optff(cache_capacity, requests)]
-        fifo_and_lru_miss_hit_results = test_fifo_and_lru_caches(caches[:-1], requests)
-        miss_stats = calculate_fifo_and_lru_stats(fifo_and_lru_miss_hit_results)
-        miss_stats['OPTFF'] = caches[-1].process_requests()
-        print(f"File {i} (miss): {miss_stats}")
+def main(input_filename: Path) -> None:
+    """
+        Creates the cache eviction policies, calculates their misses, and writes them out into the desired format
+    """
+    cache_capacity, requests = read_file(input_filename)
+
+    fifo_cache: fifo = fifo(cache_capacity)
+    fifo_misses: int = get_cache_misses(fifo_cache, requests)
+
+    lru_cache: lru = lru(cache_capacity)
+    lru_misses: int = get_cache_misses(lru_cache, requests)
+
+    optff_cache: optff = optff(cache_capacity, requests)
+    optff_misses: int = get_cache_misses(optff_cache)
+
+    miss_stats: dict[str, int] = {
+        "FIFO": fifo_misses,
+        "LRU": lru_misses,
+        "OPTFF": optff_misses
+    }
+
+    # ./inputs/example1.in --> ./outputs/example1.out
+    output_dir: str = "./outputs/"
+    output_file_base_name: str = str(input_filename.stem)
+    output_extension: str = ".out"
+    output_file: Path = Path(output_dir + output_file_base_name + output_extension)
+
+    # Write the caches and their misses to the output file
+    with open(output_file, "w") as f:
+        out_string: str = ""
+        for cache, miss in miss_stats.items():
+            out_string += f"{cache:<7}: {miss:>3}\n"  # F-string formatting for prettier outputs
+        f.write(out_string.strip())
 
 
 if __name__ == "__main__":
-    main()
+    parser: ArgumentParser = ArgumentParser(description="Script for comparing the number of misses for the FIFO, LRU, and OPTFF cache eviction policies.")
+
+    parser.add_argument(
+        "filename",
+        type=str,
+        help="The name of the input file to parse."
+    )
+
+    args = parser.parse_args()
+    input_dir: str = "./inputs/"
+
+    main(Path(input_dir + args.filename))
 
 
